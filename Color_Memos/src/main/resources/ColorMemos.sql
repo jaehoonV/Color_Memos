00e406f35d37eddbb0f3e3211b901b95a2a3cd83
jaehoon <ljh1032112@gmail.com>
2022-10-03 17:06:05 +0900

    oracle 21c, JDK-19 settings

--- Color_Memos/src/main/resources/ColorMemos.sql
+++ Color_Memos/src/main/resources/ColorMemos.sql
@@ -14,19 +14,50 @@ commit;
 
 -- 테이블 삭제
 DROP TABLE MEMBER CASCADE CONSTRAINTS;
+DROP TABLE MEMO CASCADE CONSTRAINTS;
 
 -- 테이블 생성
 /* 회원 */
 CREATE TABLE MEMBER (
-	USER_ID VARCHAR2(4000) NOT NULL, /* 회원아이디 */
+	EMAIL VARCHAR2(1000) NOT NULL, /* 회원아이디 */
 	PASSWORD VARCHAR(50) NOT NULL, /* 회원비밀번호 */
-	USER_NAME VARCHAR(50) NOT NULL /* 회원이름 */
+	USERNAME VARCHAR(50) NOT NULL /* 회원이름 */
 );
 
 ALTER TABLE MEMBER
 	ADD
-		CONSTRAINT PK_member
+		CONSTRAINT PK_MEMBER
 		PRIMARY KEY (
-			USER_ID
+			EMAIL
 		);
         
+/* 메모 */
+CREATE TABLE MEMO (
+    MNO            NUMBER(10,0) NOT NULL,      /* 메모번호 */
+    MNAME          VARCHAR2(100) NOT NULL,     /* 메모제목 */
+    MDESCRIPTION   VARCHAR2(4000) NOT NULL,    /* 메모 */
+	REGDAY         DATE,                       /* 메모작성일 */
+    REGID          VARCHAR2(1000) NOT NULL,    /* 메모작성자 */
+    MCOLOR         VARCHAR2(1000) NOT NULL,    /* 메모색상 */
+	HIDE_GB        NUMBER DEFAULT 0 NOT NULL,  /* 숨김구분 */
+    FAVORITE_GB    NUMBER DEFAULT 0 NOT NULL,  /* 즐겨찾기구분 */
+    DELETE_GB      NUMBER DEFAULT 0 NOT NULL,  /* 삭제구분 */
+    DELT_DATE      DATE,                       /* 메모삭제일 */
+	RESTORE_DATE   DATE                        /* 메모복구일 */
+);
+
+ALTER TABLE MEMO
+	ADD
+		CONSTRAINT PK_MEMO
+		PRIMARY KEY (
+			MNO
+		);
+
+-- 메모 테이블 시퀀스 MNO
+CREATE SEQUENCE MEMO_SEQ INCREMENT BY 1; 
+
+
+
+SELECT  * FROM MEMBER;
+SELECT  * FROM MEMO;
+       
